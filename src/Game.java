df3b736fdb6e92da4f3a7521a4af0763e8bd1f27
gg7767 <47157086+gg7767@users.noreply.github.com>
2024-07-06 14:13:58 -0500

    Added main function to the Game.java class

--- src/Game.java
+++ src/Game.java
@@ -12,14 +12,29 @@ public class Game {
         this.cost = cost;
     }
 
-    // Constructor with only one arg
-    Game(int population){
-        this.population = population;
-        this.hawkPop = 20;
-        this.resource = 50;
-        this.cost = 100;
+    public void start() {
+        //write the simulation here
     }
 
-    public void start() {
+    public void displayMenu() {
+        //write the menu here
+    }
+
+
+
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 4) {
+            System.err.println("Usage: ./project02 popSize [percentHawks] [resourceAmt] [costHawk-Hawk]");
+            System.exit(1);
+        }
+
+        int population = Integer.parseInt(args[0]);
+        int hawkPercent = (args.length > 1) ? Integer.parseInt(args[1]) : 20;
+        int hawkPop = (int)(population * (hawkPercent / 100.0));
+        int resourceAmt = (args.length > 2) ? Integer.parseInt(args[2]) : 50;
+        int cost = (args.length > 3) ? Integer.parseInt(args[3]) : 100;
+
+        Game game = new Game(population, hawkPop, resourceAmt, cost);
+        game.start();
     }
 }
